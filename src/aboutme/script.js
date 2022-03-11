c4d2834608861189d48cf3480b35ebc03a37101c
gskudo <gskudo@yahoo.com.br>
2022-03-11 18:20:23 -0300

    Primeira atualização

--- src/aboutme/script.js
+++ src/aboutme/script.js
@@ -1,1 +1,15 @@
-const $ = require( "jquery" )( window );
+getText("https://simple-market-backend.herokuapp.com/aboutme")
+
+async function getText(file) {
+  let myObject = await fetch(file);
+  let myText = await myObject.text();
+  var leodata = JSON.parse(myText);
+
+  document.getElementById("greeting").innerHTML  = leodata.greeting;
+  document.getElementById("aboutme").innerHTML  = leodata.aboutMe;
+  document.getElementById("email").innerHTML  = leodata.email;
+  document.getElementById("linkedin").innerHTML  = leodata.linkedin;
+  document.getElementById("mobile").innerHTML  = leodata.mobileNumber;
+}
+
+
