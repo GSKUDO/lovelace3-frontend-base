f9db45857a117186edf1089f15201b1f07d55b8f
Gislene Sayuri <gskudo@yahoo.com.br>
2022-03-25 14:50:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const app = express();
 const port = 8080;
 
 app.use('/', express.static('src/home'));
-app.use('/aboutme', express.static('src/aboutme'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
